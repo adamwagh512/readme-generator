c85b50b43c9eaebe9f861d171d7196c9a1326ab4
Adam Wagh <adamwagh@gmail.com>
2022-07-22 17:55:37 -0500

    added much of the JS, Generate Readme is working, just need to add badges and table of contents

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,132 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs')
+var badge =''
+var link = ''
+var section =''
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+return inquirer.prompt([
+  {
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'projectTitle',
+  },
+  {
+    type: 'input',
+    message: 'Please describe your project',
+    name: 'purpose'
+  },
+  {
+    type: 'input',
+    message: 'How do you install this app?',
+    name: 'installInstructions'
+  },
+  {
+    type: 'input',
+    message: 'How do you use this app',
+    name: 'useInstructions'
+  },
+  {
+    type: 'input',
+    message: 'How can somebody contribute to this project?',
+    name: 'contribute'
+  },
+  {
+    type: 'input',
+    message: 'How can this app be tested?',
+    name: 'testing'
+  },
+  {
+    type:'list',
+    message: 'What kind of license would you like to use?',
+    name: 'license',
+    choices: ['none', 'MIT', 'Apache License 2.0'],
+  },
+  {
+    type: 'input',
+    message: 'What is your Github username?',
+    name: 'github'
+  },
+  {
+    type: 'input',
+    message: 'What is your email address?',
+    name: 'email'
+  },
+  ]);
+};
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const generateReadMe = ({projectTitle, purpose, installInstructions, useInstructions, contribute, testing, licenseType, github, email, badge, link, section}) =>
+`# ${projectTitle}
+${badge}
+## Table of Contents 
+Description
+Installation Instruction
+Usage Instructions
+License 
+Contributing
+Testing
+Questions
 
-// TODO: Create a function to initialize app
-function init() {}
+## Description
+${purpose}
 
-// Function call to initialize app
-init();
+## Installation Instruction
+${installInstructions}
+
+## User Instruction 
+${useInstructions}
+
+## License 
+${licenseType}
+
+## Contributing
+${contribute}
+
+## Testing
+${testing}
+
+## License 
+${section}
+${link}
+
+## Questions
+If you have any questions email me at: ${email}
+Check out my Github page: www.github.com/${github}`;
+
+const init = () => {
+  promptUser()
+    .then((answers) => fs.writeFileSync('README1.md', generateReadMe(answers)))
+    .then(() =>console.log('Successfully wrote to README1.md'))
+    .catch((err) => console.log(err))
+  };
+
+  init()
+
+//   .then((answers) => {
+//     // Use user feedback for... whatever!!
+//   })
+//   .catch((error) => {
+//     if (error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else went wrong
+//     }
+//   });
+// };
+// // TODO: Create an array of questions for user input
+// const questions = [];
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+// //       err ? console.log(err) : console.log('Success!')
+// //     );
+
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,15 +1,69 @@
+const index = require('../index')
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license){
+    case 'MIT':
+    badge = [![License: MIT](`https://img.shields.io/badge/License-MIT-yellow.svg`)](`https://opensource.org/licenses/MIT`)
+    break
+    
+    case 'Apache License 2.0':
+    console.log();
+    break
+
+    case 'none':
+    console.log();
+    break
+
+    default:
+      console.log('Error, could not render license badge')
+}
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license){
+    case 'MIT':
+    link = `https://opensource.org/licenses/MIT`
+    break
+    
+    case 'Apache License 2.0':
+    Section = ''
+    break
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+    case 'none':
+    console.log();
+    break
 
+    default:
+      console.log('Error, could not render license link')
+}
+}
+
+// TODO: Create a function that returns the license section of README If there is no license, return an empty string
+function renderLicenseSection(license) {
+  switch (license){
+    case 'MIT':
+      Section = `Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+      The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+      
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`
+    break
+    
+    case 'Apache License 2.0':
+    console.log();
+    break
+
+    case 'none':
+    console.log();
+    break
+
+    default:
+      console.log('Error, could not render license section')
+}
+};
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
@@ -17,4 +71,4 @@ function generateMarkdown(data) {
 `;
 }
 
-module.exports = generateMarkdown;
+module.exports = renderLicenseBadge(), renderLicenseLink();
